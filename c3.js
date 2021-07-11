355c1e527358775cb37af037b184c182ed99c9e7
Sagar Saxena <47736205+Slayee@users.noreply.github.com>
2021-07-11 13:25:07 +0530

    Update c3.js

--- c3.js
+++ c3.js
@@ -13,7 +13,7 @@
 function isEven(num){
     if(num == 0) return true;
     else if(num == 1) return false;
-    else if(num < 0) return isEven(-num);//Doubt find whether a negative no. can be even or odd.
+    else if(num < 0) return isEven(-num);
     else return isEven( num - 2 );
     
 }
@@ -23,23 +23,23 @@ console.log(isEven(prompt()));
 
 // Bean Counting
 
-// function countBs(string){
-//     let bs = 0;
-//     if (typeof string == "string"){
-//         for (let n = 0; n < string.length; n++) {
-//             if(string[n] === "B") bs++;
-//         }
-//         return bs;
-//     }
-// }
-// function countChar(string, char){
-//     let num = 0;
-//     if (typeof string == "string"){
-//         for (let n = 0; n < string.length; n++) {
-//             if(string[n] === char) num++;
-//         }
-//         return num;
-//     }
-// }
+function countBs(string){
+    let bs = 0;
+    if (typeof string == "string"){
+        for (let n = 0; n < string.length; n++) {
+            if(string[n] === "B") bs++;
+        }
+        return bs;
+    }
+}
+function countChar(string, char){
+    let num = 0;
+    if (typeof string == "string"){
+        for (let n = 0; n < string.length; n++) {
+            if(string[n] === char) num++;
+        }
+        return num;
+    }
+}
 
-// console.log(countChar("sagar is a good slayee", "i"));
+console.log(countChar("Sagar is FrontEndProgrammer", "m"));
